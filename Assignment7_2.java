8a2d8eb94c53682b66259c2d00db29df43634608
ShiJiaHan <55767004+ShiJiaHan@users.noreply.github.com>
2021-12-05 21:39:59 -0800

    Update Assignment7_2.java

--- Assignment7_2.java
+++ Assignment7_2.java
@@ -37,7 +37,7 @@ public class Assignment7_2
         int prime2 = 0;
         
         while (index < primes.size()){
-            prime1=primes.get(index);
+            prime1 = primes.get(index);
             if (primes.indexOf(n-prime1) != -1){
             prime2 = n-prime1;
             break;
@@ -51,7 +51,7 @@ public class Assignment7_2
     }
     
     public static ArrayList<Integer> add(ArrayList<Integer> bigInt1,ArrayList<Integer> bigInt2){
-        ArrayList<Integer> ans=new ArrayList<Integer>();
+        ArrayList<Integer> ans = new ArrayList<Integer>();
         int index1 = bigInt1.size() - 1;
         int index2 = bigInt2.size() - 1;
         int addingOne = 0;
